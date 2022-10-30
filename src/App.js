4e56242509ff1bcb195a045d3f860b47a12840ba
omoloye13 <omoloyefauziyat@gmail.com>
2022-10-30 21:03:07 +0000

    setup of the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ZuriButton } from './Component/Button';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello</h1>
+     <ZuriButton />
     </div>
   );
 }
---
+++ src/Component/Button.js
@@ -0,0 +1,47 @@
+export const TwitterButton =()=>{
+    return(
+        <>
+        
+        </>
+    );
+}
+
+
+export const ZuriButton =()=>{
+    return(
+        <>
+        <div>
+            <a href="https://training.zuri.team/" id="btn_zuri"  className="btn">Zuri Team</a>
+        </div>
+        
+        </>
+    );
+}
+export const BookButton =()=>{
+    return(
+        <>
+        
+        </>
+    );
+}
+export const pythonButton =()=>{
+    return(
+        <>
+        
+        </>
+    );
+}
+export const PitchButton =()=>{
+    return(
+        <>
+        
+        </>
+    );
+}
+export const DesignButton =()=>{
+    return(
+        <>
+        
+        </>
+    );
+}
